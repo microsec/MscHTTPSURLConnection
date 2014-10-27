72dafcf8a89ae41fefb4929501a5cf7fbcda0313
Lendvai Richárd <richy@Lendvai-MacBook-Pro.local>
2014-10-27 09:41:53 +0100

    Initial Commit

---
+++ MscHTTPSURLConnection/MscHTTPSURLConnection.h
@@ -0,0 +1,13 @@
+//
+//  MscHTTPSURLConnection.h
+//  MscHTTPSURLConnection
+//
+//  Created by Lendvai Richárd on 2014.10.27..
+//  Copyright (c) 2014 Lendvai Richárd. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MscHTTPSURLConnection : NSObject
+
+@end
---
+++ MscHTTPSURLConnection/MscHTTPSURLConnection.m
@@ -0,0 +1,13 @@
+//
+//  MscHTTPSURLConnection.m
+//  MscHTTPSURLConnection
+//
+//  Created by Lendvai Richárd on 2014.10.27..
+//  Copyright (c) 2014 Lendvai Richárd. All rights reserved.
+//
+
+#import "MscHTTPSURLConnection.h"
+
+@implementation MscHTTPSURLConnection
+
+@end
